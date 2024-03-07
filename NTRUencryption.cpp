ecb5a6b0e43d9ab23235e7e9559e01fcf27697ad
Fernando Osorio <alexis.fernando.osorio.sarabio@gmail.com>
2024-03-07 17:05:59 -0600

    Routines for finding inverses coded. Division algorithm started

--- NTRUencryption.cpp
+++ NTRUencryption.cpp
@@ -1,16 +1,15 @@
 #include"NTRUencryption.hpp"
 
 NTRUencryption::NTRUPolynomial NTRUencryption::NTRUPolynomial::operator +
-(const NTRUPolynomial& t) const{
+(const NTRUPolynomial& t) const {
     NTRUencryption::NTRUPolynomial r;                                           // Initializing with zeros
-    for(int i = 0; i < N; i++) {
+    for(int i = 0; i < N; i++)
         r.coefficients[i] = modq(this->coefficients[i] + t.coefficients[i]);       // Addition element by element. The addition is performed in the Zp group
-    }
     return r;
 }
 
 NTRUencryption::NTRUPolynomial NTRUencryption::NTRUPolynomial::operator *
-(const NTRUPolynomial& t) const{
+(const NTRUPolynomial& t) const {
     NTRUencryption::NTRUPolynomial r;                                           // Initializing with zeros
     int i, j;
     for(i = 0; i < N; i++) {                                                    // Convolution process
@@ -30,19 +29,49 @@ NTRUPolynomial result[2]) const {
         ". Division by zero...\n";
         return;
     }
-}
-
-NTRUencryption::NTRUPolynomial& NTRUencryption::NTRUPolynomial::operator =
-(const NTRUPolynomial& t) {
-    if(this != &t) {                                                            // Guarding against self assignment
-        delete [] this->coefficients;
-        this->coefficients = new int[N];                                         // Creating array in the heap and updating degree
-        for(int i=0; i < N; i++) this->coefficients[i] = t.coefficients[i];
+    if(*this == 0) {                                                            // Case zero divided by anything
+        result[0] = 0; result[1] = 0;
+        return;
     }
-    return *this;
+
+    const int dividendDegree = this->degree();
+    const int divisorDegree  = t.degree();
+    int degreeDiff;                                                              // Difference between degrees
+    int remainderDegree;
+    int leadCoeffDivndInv;                                                       // Inverse (modulus q) of leading coefficient of the dividend
+
+    if(dividendDegree < divisorDegree) {                                        // Case dividend has smaller degree than divisor
+        result[0] = 0; result[1] = *this;
+        return;
+    }
+    try{
+        leadCoeffDivndInv = invModq(this->coefficients[dividendDegree]);
+    } catch(char* excp) {
+        std::cout << "\nIn NTRUencryption.cpp, function void NTRUencryption::"
+        "NTRUPolynomial::division(const NTRUPolynomial t,NTRUPolynomial resul"
+        "t[2]) const\n";
+        throw excp;
+    }                                                                           // At this point we know leading coefficient has an inverse in Zq
+    degreeDiff = dividendDegree - divisorDegree;                                 // At this point we know degreeDiff >= 0
+    remainderDegree = divisorDegree;
+    result[1] = *this;                                                          // Initializing remainder with dividend (this)
+    result[0] = 0;                                                              // Setting quotient as zero
+
+    while(degreeDiff >= 0) {
+        result[0].coefficients[degreeDiff] =
+        leadCoeffDivndInv * result[1].coefficients[remainderDegree];
+    }
 }
 
-int NTRUencryption::modq(int t) {
-    while(t < 0) t += q;                                                        // Adding q till we get t positive. This won't affect the result
-    return t &= q-1;                                                            // Since q is a power of 2, the expression t &= q-1 is equivalent to t %= q
+int NTRUencryption::invModq(int t) {
+    const int exp = (q >> 1) - 1;											    // exp = q/2 - 1
+	int bit = 1;															    // Single bit; it will 'run' trough the bits of exp
+	int r = (t = modq(t));                                                      // Making sure 0 <= t < q. Assigning to r
+	if((t & 1) == 0) throw "\nIn NTRUencryption.cpp, function int NTRUencrypti"
+	"on::invModq(int t). No inverse modulus q for even numbers.\n";
+	for(; (exp & bit) != 0; bit <<= 1) {                                        // Using exponentiation algorithm to find the inverse
+		(r *= r) *= t;                                                          // Enhanced for this particular case (q-1 has the form 111...111)
+		r = modq(r);
+	}
+	return r;
 }
--- NTRUencryption.hpp
+++ NTRUencryption.hpp
@@ -1,24 +1,16 @@
 // Operating with the polynomials in the ring Zp[x]/x^N-1.
 #include<iostream>
 
-
 class NTRUencryption {
 	public:
-	enum NTRU_N {_509 = 509, _677 = 677, _701 = 701, _821 = 821};				// All the possible values for the N
-	enum NTRU_q {_2048 = 2048, _4096 = 4096, _8192 = 8192};						// All the possible values for the q
+	enum NTRU_N {_509_  = 509,  _677_  = 677,  _701_  = 701,  _821_ = 821};		// All the possible values for the N
+	enum NTRU_q {_2048_ = 2048, _4096_ = 4096, _8192_ = 8192};					// All the possible values for the q
 	static const NTRU_N N;
 	static const NTRU_q q;
 
 	private:																	// Private types and attributes
 	enum log2NTRUq {_11 = 11, _12 = 12, _13 = 13};								// For each possible q: log_2(q)
 	static const log2NTRUq L2NTRU_q;
-
-	static const int _3inverseMod2048 = 683;									//  3*683  mod 2048 = 1
-	static const int _neg3inverseMod2048 = 1365;								// -3*1365 mod 2048 = 1
-	static const int _3inverseMod4096 = 2731;									//  3*2731 mod 4096 = 1
-	static const int _neg3inverseMod4096 = 1365;								// -3*1365 mod 4096 = 1
-	static const int _3inverseMod8192 = 2731;									//  3*2731 mod 8192 = 1
-	static const int _neg3inverseMod8192 = 5461;								// -3*5461 mod 8192 = 1
 
 	struct NTRUPolynomial {														// Representation of the polynomials
 		int* coefficients = NULL;												// Coefficients of the polynomial
@@ -35,28 +27,43 @@ class NTRUencryption {
 		NTRUPolynomial operator * (const NTRUPolynomial&) const;				// Multiplication will coincide with convolution
 		void division(const NTRUPolynomial t, NTRUPolynomial result[2]) const;	// Computes quotient and remainder between this and t, saves the result in result[2]
 
-		NTRUPolynomial& operator = (const NTRUPolynomial&);						// Assignment
-
+		inline NTRUPolynomial& operator = (const NTRUPolynomial& t) {			// Assignment
+			if(this != &t)                                                    	// Guarding against self assignment
+        		for(int i=0; i<N; i++) this->coefficients[i] = t.coefficients[i];
+    		return *this;
+		}
+		inline NTRUPolynomial& operator = (int t) {								// Assignment with single integer
+        	for(int i=0; i < N; i++) this->coefficients[i] = t;
+    		return *this;
+		}
 		inline bool operator == (int t) const {									// Comparison with a single integer
 			return this->degree() == 0 && this->coefficients[0] == t;
 		}
-
 		inline int degree() const {												// Returns degree of polynomial
 			int deg = N;
-			while(this->coefficients[--deg] == 0) {}
+			while(this->coefficients[--deg] == 0 && deg > 0) {}
 			return deg;
 		}
+	};
+	inline static int modq(int t) {												// returns t mod q using bit wise operations. The result will be positive
+		while(t < 0) t += q;													// Adding q till we get t positive. This won't affect the result
+    	return t &= q-1;														// Since q is a power of 2, the expression t &= q-1 is equivalent to t %= q
+	}
 
-		private:
-		inline int min(int a, int b) {											// Returns the smaller of two numbers
-			if(a < b) return a;
-			return b;
+	static int invModq(int t);													// Calculates inverse modulus q}
+
+	inline static int _3inverseModq(NTRU_q _q_) {
+		switch(_q_) {
+			case _2048_:return 683;												//  3*683  mod 2048 = 1
+			case _4096_:return 2731;											//  3*2731 mod 4096 = 1
+			case _8192_:return 2731;											//  3*2731 mod 8192 = 1
 		}
-	};
-
-	struct NTRUPolynomialPair {													// Array of NTRUPolynomials with just two elements
-		NTRUPolynomial polynomial[2];
-	};
-
-	static int modq(int t);														// returns t mod q using bit wise operations. The result will be positive
+	}
+	inline static int neg3inverseModq(NTRU_q _q_) {
+		switch(_q_) {
+			case _2048_:return 1365;											// -3*1365 mod 2048 = 1
+			case _4096_:return 1365;											// -3*1365 mod 4096 = 1
+			case _8192_:return 5461;											// -3*5461 mod 8192 = 1
+		}
+	}
 };
