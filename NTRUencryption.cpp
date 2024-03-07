562099ee2f02e4cf57a206079f02b61919a45f87
Fernando Osorio <alexis.fernando.osorio.sarabio@gmail.com>
2024-03-06 21:31:37 -0600

    Coefficients added. Functions created

--- NTRUencryption.cpp
+++ NTRUencryption.cpp
@@ -2,14 +2,34 @@
 
 NTRUencryption::NTRUPolynomial NTRUencryption::NTRUPolynomial::operator +
 (const NTRUPolynomial& t) const{
-    NTRUencryption::NTRUPolynomial r;
+    NTRUencryption::NTRUPolynomial r;                                           // Initializing with zeros
     for(int i = 0; i < N; i++) {
-        r.coefficients[i] = this->coefficients[i] + t.coefficients[i];
-        if(r.coefficients[i] >= 0) r.coefficients[i] &= q-1;                      // Since q is a power of 2, &= q-1 is equivalent to %= q
-        else if(r.coefficients[i] <= -q)
-                r.coefficients[i] = -(-r.coefficients[i] & q-1);                  // In the negative case, we first change to positive and then we apply modulus
+        r.coefficients[i] = modq(this->coefficients[i] + t.coefficients[i]);       // Addition element by element. The addition is performed in the Zp group
     }
     return r;
+}
+
+NTRUencryption::NTRUPolynomial NTRUencryption::NTRUPolynomial::operator *
+(const NTRUPolynomial& t) const{
+    NTRUencryption::NTRUPolynomial r;                                           // Initializing with zeros
+    int i, j;
+    for(i = 0; i < N; i++) {                                                    // Convolution process
+        for(j = 0; j <= i; j++)
+            r.coefficients[i] += modq(this->coefficients[j]*t.coefficients[i-j]);  // Some optimization through a 64 bits buffer can be implemented here
+        for(; j < N; j++)
+            r.coefficients[i] += modq(this->coefficients[j]*t.coefficients[N+i-j]);// And here
+    }
+    return r;
+}
+
+void NTRUencryption::NTRUPolynomial::division(const NTRUPolynomial t,
+NTRUPolynomial result[2]) const {
+    if(t == 0) {
+        throw "\nIn NTRUencryption.cpp, function void NTRUencryption::NTRUPolyn"
+        "omial::division(const NTRUPolynomial t,NTRUPolynomial result[2]) const"
+        ". Division by zero...\n";
+        return;
+    }
 }
 
 NTRUencryption::NTRUPolynomial& NTRUencryption::NTRUPolynomial::operator =
@@ -20,4 +40,9 @@ NTRUencryption::NTRUPolynomial& NTRUencryption::NTRUPolynomial::operator =
         for(int i=0; i < N; i++) this->coefficients[i] = t.coefficients[i];
     }
     return *this;
+}
+
+int NTRUencryption::modq(int t) {
+    while(t < 0) t += q;                                                        // Adding q till we get t positive. This won't affect the result
+    return t &= q-1;                                                            // Since q is a power of 2, the expression t &= q-1 is equivalent to t %= q
 }
--- NTRUencryption.hpp
+++ NTRUencryption.hpp
@@ -1,16 +1,24 @@
 // Operating with the polynomials in the ring Zp[x]/x^N-1.
-#include <iostream>
+#include<iostream>
+
 
 class NTRUencryption {
 	public:
 	enum NTRU_N {_509 = 509, _677 = 677, _701 = 701, _821 = 821};				// All the possible values for the N
 	enum NTRU_q {_2048 = 2048, _4096 = 4096, _8192 = 8192};						// All the possible values for the q
+	static const NTRU_N N;
+	static const NTRU_q q;
 
 	private:																	// Private types and attributes
 	enum log2NTRUq {_11 = 11, _12 = 12, _13 = 13};								// For each possible q: log_2(q)
-	static const NTRU_N N;
-	static const NTRU_q q;
 	static const log2NTRUq L2NTRU_q;
+
+	static const int _3inverseMod2048 = 683;									//  3*683  mod 2048 = 1
+	static const int _neg3inverseMod2048 = 1365;								// -3*1365 mod 2048 = 1
+	static const int _3inverseMod4096 = 2731;									//  3*2731 mod 4096 = 1
+	static const int _neg3inverseMod4096 = 1365;								// -3*1365 mod 4096 = 1
+	static const int _3inverseMod8192 = 2731;									//  3*2731 mod 8192 = 1
+	static const int _neg3inverseMod8192 = 5461;								// -3*5461 mod 8192 = 1
 
 	struct NTRUPolynomial {														// Representation of the polynomials
 		int* coefficients = NULL;												// Coefficients of the polynomial
@@ -25,8 +33,19 @@ class NTRUencryption {
 																				// Arithmetic
 		NTRUPolynomial operator + (const NTRUPolynomial&) const;				// Addition element by element
 		NTRUPolynomial operator * (const NTRUPolynomial&) const;				// Multiplication will coincide with convolution
+		void division(const NTRUPolynomial t, NTRUPolynomial result[2]) const;	// Computes quotient and remainder between this and t, saves the result in result[2]
 
-		NTRUPolynomial& operator = (const NTRUPolynomial&);
+		NTRUPolynomial& operator = (const NTRUPolynomial&);						// Assignment
+
+		inline bool operator == (int t) const {									// Comparison with a single integer
+			return this->degree() == 0 && this->coefficients[0] == t;
+		}
+
+		inline int degree() const {												// Returns degree of polynomial
+			int deg = N;
+			while(this->coefficients[--deg] == 0) {}
+			return deg;
+		}
 
 		private:
 		inline int min(int a, int b) {											// Returns the smaller of two numbers
@@ -35,7 +54,9 @@ class NTRUencryption {
 		}
 	};
 
-	struct NTRUPolynomial_2array {												// Array of NTRUPolynomials with just two elements
+	struct NTRUPolynomialPair {													// Array of NTRUPolynomials with just two elements
 		NTRUPolynomial polynomial[2];
 	};
+
+	static int modq(int t);														// returns t mod q using bit wise operations. The result will be positive
 };
