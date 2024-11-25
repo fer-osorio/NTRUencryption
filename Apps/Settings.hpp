964304ba2534be38768dde95ca8bbd99d79bb8b1
Fernando Osorio <alexis.fernando.osorio.sarabio@gmail.com>
2024-11-24 21:18:47 -0600

    Macros passed to the cpp file

--- Apps/Settings.hpp
+++ Apps/Settings.hpp
@@ -2,10 +2,7 @@
 
 #ifndef SETTINGS_HPP
 #define SETTINGS_HPP
-
 #define NAME_MAX_LEN 256
-#define BYTES_FOR_FILE_SIZE 2                                                   // -Size of the information we will append to the data we intend to encrypt. Now
-#define FILE_TYPE_ID_SIZE   3                                                   //  it correspond to 3 bytes for the type of file, and tow bytes for the file size
 
 struct TXT {
 	char	name[NAME_MAX_LEN] = {0};					// -Naming instance
