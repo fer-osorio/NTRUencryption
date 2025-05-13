531b71ae5e8b88ea8ebe7c3ce83034520b5cc0d5
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-05-13 12:45:36 -0600

    New option added

--- Apps/Statistics.cpp
+++ Apps/Statistics.cpp
@@ -18,7 +18,7 @@ static std::string selectNTRU_q =
 std::string("Select NTRU_q parameter:\n") + NTRU_q_valuesList + "\n";
 
 static std::string statsCategory =
-"What statistics do you want to get?\n(0) Key Generation time \n(1) Ciphering and deciphering time \n(2) Encrypted data\n(3) All\n";
+"What statistics do you want to get?\n(0) Key Generation time \n(1) Ciphering and deciphering time\n(2) Key Generation time, ciphering and deciphering time \n(3) Encrypted data\n(4) All\n";
 
 static const char invalidInputMsg[] = "\nInvalid input. Try again.\n";
 
@@ -48,7 +48,7 @@ static int retreaveValidOption(std::string optionsString, bool (validOptionCrite
 
 static bool NTRU_N_validOpt(int opt) { return opt >= 0 && opt < (int)NTRU_N_amount; }
 static bool NTRU_q_validOpt(int opt) { return opt >= 0 && opt < (int)NTRU_q_amount; }
-static bool statisticCategory(int opt) { return opt >= 0 && opt < 4; }
+static bool statisticCategory(int opt) { return opt >= 0 && opt < 5; }
 
 int main(int argc, const char* argv[]){
     if(argc > 1) {
@@ -84,6 +84,15 @@ int main(int argc, const char* argv[]){
             std::cout << std::endl;
             break;
         case 2:
+            std::cout << "Computing Statistics::Time::keyGeneration(" << N << ", " << q << ")\n";
+            std::cout << "Computing Statistics::Time::ciphering(" << N << ", " << q << ")\n";
+            std::cout << "Computing Statistics::Time::deciphering(" << N << ", " << q << ")\n";
+            kg = NTRU::Encryption::Statistics::Time::keyGeneration(N,q);
+            ch = NTRU::Encryption::Statistics::Time::ciphering(N,q);
+            dc = NTRU::Encryption::Statistics::Time::deciphering(N,q);
+            std::cout << std::endl;
+            break;
+        case 3:
             std::cout << "Computing Statistics::Data::encryption(" << N << ", " << q << ")\n";
             dt = NTRU::Encryption::Statistics::Data::encryption(N,q);
             std::cout << std::endl;
@@ -102,7 +111,7 @@ int main(int argc, const char* argv[]){
     std::cout << "Parameters:: N = "<< N << ", q = " << q << " ------------------------------------" << std::endl;
     std::cout << std::fixed << std::setprecision(5) <<std::endl;
 
-    if(opt_C == 0 || opt_C == 3){
+    if(opt_C == 0 || opt_C == 2 || opt_C == 4){
         std::cout << "Key Generation time statistics:" << std::endl;
         std::cout << "Maximum: " << kg.getMaximum() << '\n';
         std::cout << "Minimum: " << kg.getMinimum() << '\n';
@@ -111,7 +120,7 @@ int main(int argc, const char* argv[]){
         std::cout << "Average Absolute Deviation: " << kg.getAAD() << '\n' << std::endl;
     }
 
-    if(opt_C == 1 || opt_C == 3){
+    if(opt_C == 1 || opt_C == 2 || opt_C == 4){
         std::cout << "Encryption time statistics:" << std::endl;
         std::cout << "Maximum: " << ch.getMaximum() << '\n';
         std::cout << "Minimum: " << ch.getMinimum() << '\n';
@@ -127,7 +136,7 @@ int main(int argc, const char* argv[]){
         std::cout << "Average Absolute Deviation: " << dc.getAAD() << '\n' << std::endl;
     }
 
-    if(opt_C == 2 || opt_C == 3){
+    if(opt_C == 3 || opt_C == 4){
         std::cout << "Encrypted data statistics: " << std::endl;
         std::cout << "Entropy: " << dt.getEntropy() << '\n';
         std::cout << "Correlation: " << dt.getCorrelation() << '\n';
