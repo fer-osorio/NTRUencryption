64e66646ff4cf2d7460dde07179dcfb595e1b303
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-06-17 22:47:51 -0600

    File updated. Exception handling mechanisms added

--- Apps/Statistics.cpp
+++ Apps/Statistics.cpp
@@ -35,19 +35,44 @@ static int retreaveValidOption(std::string optionsString, bool (validOptionCrite
 static bool statisticCategory(int opt) { return opt >= 0 && opt < 5; }
 
 int main(int argc, const char* argv[]){
-    if(argc > 1) {
-        std::cout << "Executable does not support arguments," << std::endl;
-        std::cout << "I will ignore: ";
-        for(int i = 1; i < argc; i++) std::cout << argv[i] << ' ';
-        std::cout  << std::endl;
-    }
-    int opt_C = retreaveValidOption(statsCategory, statisticCategory);
-
+    NTRU::Encryption* ptr_e = NULL;
     NTRU::Encryption::Statistics::Time kg;
     NTRU::Encryption::Statistics::Time ch, dc;
     NTRU::Encryption::Statistics::Data dt;
+    int opt_C = retreaveValidOption(statsCategory, statisticCategory);
+    switch(argc) {                                                              // -Accept one parameter, intended for the private key
+        case 1:
+            if(opt_C != 0){                                                     // -If no argument passed and if not just key creation statistics
+                try {                                                           //  needed,the program will generate a encryption object
+                    ptr_e = new NTRU::Encryption();
+                } catch(const std::runtime_error& exp){
+                    std::cerr << "Could not create NTRU::Encryption object.\n" << exp.what() << '\n';
+                    return EXIT_FAILURE;
+                }
+            }
+            break;
+        case 2:
+            try{
+                ptr_e = new NTRU::Encryption(argv[1]);                          // -Creating encryption object from (supposedly) private key file
+            } catch(const std::runtime_error& exp){
+                std::cerr << "\nCould not create NTRU::Encryption object from file.\n\n" << exp.what() << '\n';
+                return EXIT_FAILURE;
+            }
+            if(!ptr_e->validPrivateKeyAvailable()){                             // -Validating polynomial passed as private key.
+                std::cerr << "\n\nExecutable argument must refer to a valid private key. Terminating the program with FAILURE status.\n\n";
+                return EXIT_FAILURE;
+            }
+            break;
+        default:
+            std::cerr << "\n\nExecutable accept one and just one argument. Termination the program with FAILURE status.\n\n";
+            return EXIT_FAILURE;
+    }
 
-    std::cout << '\n';
+    if(ptr_e != NULL) {
+        std::cout << "Running test with:" << '\n';
+        ptr_e->printKeys("Public key","Private key");
+        std::cout << std::endl;
+    }
 
     switch(opt_C){
         case 0:
@@ -58,8 +83,8 @@ int main(int argc, const char* argv[]){
         case 1:
             std::cout << "Computing Statistics::Time::ciphering()\n";
             std::cout << "Computing Statistics::Time::deciphering()\n";
-            ch = NTRU::Encryption::Statistics::Time::ciphering();
-            dc = NTRU::Encryption::Statistics::Time::deciphering();
+            ch = NTRU::Encryption::Statistics::Time::ciphering(ptr_e);
+            dc = NTRU::Encryption::Statistics::Time::deciphering(ptr_e);
             std::cout << std::endl;
             break;
         case 2:
@@ -67,31 +92,31 @@ int main(int argc, const char* argv[]){
             std::cout << "Computing Statistics::Time::ciphering()\n";
             std::cout << "Computing Statistics::Time::deciphering()\n";
             kg = NTRU::Encryption::Statistics::Time::keyGeneration();
-            ch = NTRU::Encryption::Statistics::Time::ciphering();
-            dc = NTRU::Encryption::Statistics::Time::deciphering();
+            ch = NTRU::Encryption::Statistics::Time::ciphering(ptr_e);
+            dc = NTRU::Encryption::Statistics::Time::deciphering(ptr_e);
             std::cout << std::endl;
             break;
         case 3:
             std::cout << "Computing Statistics::Data::encryption()\n";
-            dt = NTRU::Encryption::Statistics::Data::encryption();
+            dt = NTRU::Encryption::Statistics::Data::encryption(ptr_e);
             std::cout << std::endl;
             break;
         default:
             std::cout << "Computing Statistics::Time::keyGeneration()\n";
             std::cout << "Computing Statistics::Time::ciphering()\n";
             std::cout << "Computing Statistics::Data::encryption()\n";
+            std::cout << "Computing Statistics::Time::deciphering()\n";
             kg = NTRU::Encryption::Statistics::Time::keyGeneration();
-            ch = NTRU::Encryption::Statistics::Time::ciphering();
-            dc = NTRU::Encryption::Statistics::Time::deciphering();
-            dt = NTRU::Encryption::Statistics::Data::encryption();
+            ch = NTRU::Encryption::Statistics::Time::ciphering(ptr_e);
+            dc = NTRU::Encryption::Statistics::Time::deciphering(ptr_e);
+            dt = NTRU::Encryption::Statistics::Data::encryption(ptr_e);
             break;
     }
-
-    //std::cout << "Parameters:: N = "<< N << ", q = " << q << " ------------------------------------" << std::endl;
+    if(ptr_e != NULL) { delete ptr_e; ptr_e = NULL; }                           // Encryption object not usefull anymore.
     std::cout << std::fixed << std::setprecision(5) << std::endl;
 
     if(opt_C == 0 || opt_C == 2 || opt_C == 4){
-        std::cout << "\nKey Generation time statistics:" << std::endl;
+        std::cout << "\nKey Generation time statistics (microseconds):" << std::endl;
         std::cout << "Maximum: " << kg.getMaximum() << '\n';
         std::cout << "Minimum: " << kg.getMinimum() << '\n';
         std::cout << "Average: " << kg.getAverage() << '\n';
@@ -100,14 +125,14 @@ int main(int argc, const char* argv[]){
     }
 
     if(opt_C == 1 || opt_C == 2 || opt_C == 4){
-        std::cout << "Encryption time statistics:" << std::endl;
+        std::cout << "Encryption time statistics (microseconds):" << std::endl;
         std::cout << "Maximum: " << ch.getMaximum() << '\n';
         std::cout << "Minimum: " << ch.getMinimum() << '\n';
         std::cout << "Average: " << ch.getAverage() << '\n';
         std::cout << "Standard deviation: " << sqrt(ch.getVariance()) << '\n';
         std::cout << "Average Absolute Deviation: " << ch.getAAD() << '\n' << std::endl;
 
-        std::cout << "Decryption time statistics:" << std::endl;
+        std::cout << "Decryption time statistics (microseconds):" << std::endl;
         std::cout << "Maximum: " << dc.getMaximum() << '\n';
         std::cout << "Minimum: " << dc.getMinimum() << '\n';
         std::cout << "Average: " << dc.getAverage() << '\n';
