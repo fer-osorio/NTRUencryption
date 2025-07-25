0654d67e0e69e67e923b44c1adbdaa56ba8cd576
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-07-24 22:29:04 -0600

    File updated. ZpPolynomial -> RpPolynomial, ZqPolynomial -> RqPolynomila, Z2Polynomial -> R2Polynomial

--- include/ntru/encryption.hpp
+++ include/ntru/encryption.hpp
@@ -12,19 +12,19 @@ int get_q();
 class Encryption {
 private:
 	// -Initializing with zeros
-	ZqPolynomial publicKey	= ZqPolynomial();
-	ZpPolynomial privatKey	= ZpPolynomial();				// -Private key has the form p·F0+1, we are saving just F0
+	RqPolynomial publicKey	= RqPolynomial();
+	RpPolynomial privatKey	= RpPolynomial();				// -Private key has the form p·F0+1, we are saving just F0
 	bool validPrivateKey	= false;					// -Flag; tells us if current object can only encrypt (only have a valid publickKey)
 										// -or also is capable of decryption (has a valid private key).
 public:
 	Encryption();								// -Building keys automatically. Trows: const std::runtime_error&
 	Encryption(const char* NTRUkeyFile);					// -Building from a NTRU key file. Trows: NTRU::MathException, NTRU::FileIOException, NTRU::ParameterMismatchException
 
-	ZqPolynomial encrypt(const char bytes[] ,size_t size) const;		// -Encryption of char array
-	ZqPolynomial encrypt(const ZpPolynomial&) const;			// -Encrypts ZpPolynomial
-	ZqPolynomial encryptFile(const char fileName[]) const;			// -Reads file content, writes in array, append a 0x80 at the end and encrypts. Throws NTRU::FileIOException
-	ZpPolynomial decrypt(const ZqPolynomial&) const;			// -Decryption of ZqPolynomial
-	ZpPolynomial decrypt(const char bytes[] ,size_t size) const;		// -Decryption of char array
+	RqPolynomial encrypt(const char bytes[] ,size_t size) const;		// -Encryption of char array
+	RqPolynomial encrypt(const RpPolynomial&) const;			// -Encrypts RpPolynomial
+	RqPolynomial encryptFile(const char fileName[]) const;			// -Reads file content, writes in array, append a 0x80 at the end and encrypts. Throws NTRU::FileIOException
+	RpPolynomial decrypt(const RqPolynomial&) const;			// -Decryption of RqPolynomial
+	RpPolynomial decrypt(const char bytes[] ,size_t size) const;		// -Decryption of char array
 
 	bool validPrivateKeyAvailable() const{ return this->validPrivateKey; }
 
@@ -34,16 +34,16 @@ public:
 	static size_t privateKeySizeInBytes();
 	static size_t publicKeySizeInBytes();
 	/*
-		Remark: ZpPolynomial structure has no method to read or write files, neither to build an object from a byte array nor to write a
+		Remark: RpPolynomial structure has no method to read or write files, neither to build an object from a byte array nor to write a
 		byte array from an object; this is delegated to the encryption class because the building procedure depends on the answer to the
 		following question: The data from the byte array represents an arbitrary message waiting to be encrypted (plain text), or it
 		represents a private encryption key (private key)? The answer to these questions determines the reading and writing procedures.
 	*/
-	static ZpPolynomial ZpPolynomialFromBytes(const char bytes[], size_t size, bool isPrivateKey); // -Builds a ZpPolynomial from an array of bytes. The building procesure is determined by the bool argument 'isPrivateKey'. Throws: std::runtime_error&
-	static ZpPolynomial ZpPolynomialPlainTextFromFile(const char* fileName);// -Building a ZpPolynomial from file. Throws FileIOException
-	static void ZpPolynomialtoBytes(const ZpPolynomial& org, char dest[], bool isPrivateKey);// -From ZpPolynomial to bytes. If isPrivateKey is true, polynomial is interpreted as private key
-	static void ZpPolynomialPlainTextSave(const ZpPolynomial& org, const char* name = NULL, bool saveAsText = false);// -Saving ZpPolynomial in a file with format. It assumes the data containded inside it has no format. Throws NTRU::FileIOException
-	static void ZpPolynomialWriteFile(const ZpPolynomial& org, const char* fileName, bool writeAsText);// -Writes a file using the data contained in the polynomial. In this case, it looks for the "end-of-content" mark, which is 1000,0000. Throws NTRU::FileIOException
+	static RpPolynomial ZpPolynomialFromBytes(const char bytes[], size_t size, bool isPrivateKey); // -Builds a RpPolynomial from an array of bytes. The building procesure is determined by the bool argument 'isPrivateKey'. Throws: std::runtime_error&
+	static RpPolynomial ZpPolynomialPlainTextFromFile(const char* fileName);// -Building a RpPolynomial from file. Throws FileIOException
+	static void ZpPolynomialtoBytes(const RpPolynomial& org, char dest[], bool isPrivateKey);// -From RpPolynomial to bytes. If isPrivateKey is true, polynomial is interpreted as private key
+	static void ZpPolynomialPlainTextSave(const RpPolynomial& org, const char* name = NULL, bool saveAsText = false);// -Saving RpPolynomial in a file with format. It assumes the data containded inside it has no format. Throws NTRU::FileIOException
+	static void ZpPolynomialWriteFile(const RpPolynomial& org, const char* fileName, bool writeAsText);// -Writes a file using the data contained in the polynomial. In this case, it looks for the "end-of-content" mark, which is 1000,0000. Throws NTRU::FileIOException
 	/*	There is no "build private key from file" method because it is already implemented in the constructor Encryption(const char* NTRUkeyFile)	*/
 
 	void saveKeys(const char publicKeyName[] = NULL, const char privateKeyName[] = NULL) const; // -Saves keys using the names passed as arguments. Throws NTRU::FileIOException
@@ -51,13 +51,13 @@ public:
 
 	// -Performance measurement
 	static StatisticalMeasures::Dispersion<uint32_t> keyGenerationTime();
-	static StatisticalMeasures::Dispersion<uint32_t> ciphering(const NTRU::Encryption* e, const NTRU::ZpPolynomial* msg);// Taking average of encryption time
-	static StatisticalMeasures::Dispersion<uint32_t> deciphering(const NTRU::Encryption* e, const NTRU::ZqPolynomial* emsg);// Taking average of decryption time
+	static StatisticalMeasures::Dispersion<uint32_t> ciphering(const NTRU::Encryption* e, const NTRU::RpPolynomial* msg);// Taking average of encryption time
+	static StatisticalMeasures::Dispersion<uint32_t> deciphering(const NTRU::Encryption* e, const NTRU::RqPolynomial* emsg);// Taking average of decryption time
 	static StatisticalMeasures::DataRandomness encryptedData(const NTRU::Encryption* ptr_e);
 
 private:
-	ZqPolynomial productByPrivatKey(const ZqPolynomial& P) const;
-	ZqPolynomial productByPrivatKey(const Z2Polynomial& P) const;
+	RqPolynomial productByPrivatKey(const RqPolynomial& P) const;
+	RqPolynomial productByPrivatKey(const R2Polynomial& P) const;
 	void setKeys();								// -Creation of the keys. Throws NTRU::MathException
 	void setKeysFromPrivKey();						// -Creates private key inverse and public key from private key. Intended for the
 										//  creation of Encryption object from file. Throws NTRU::MathException
--- include/ntru/polynomials.hpp
+++ include/ntru/polynomials.hpp
@@ -1,4 +1,6 @@
 #include<gmpxx.h>
+#include<random>
+#include<ctime>
 #include"parameters.hpp"
 
 #ifndef POLYNOMIALS_HPP
@@ -6,50 +8,50 @@
 
 namespace NTRU{
 
-struct ZpPolynomial;
-struct ZqPolynomial;
-struct Z2Polynomial;
+struct RpPolynomial;
+struct RqPolynomial;
+struct R2Polynomial;
 class  Encryption;
 
-ZqPolynomial convolutionZq(const Z2Polynomial&, const ZpPolynomial&);
-ZqPolynomial convolutionZq(const Z2Polynomial&, const ZqPolynomial&);
-ZqPolynomial convolutionZq(const ZpPolynomial&, const ZqPolynomial&);
-ZpPolynomial mods_p(ZqPolynomial);
+RqPolynomial convolutionZq(const R2Polynomial&, const RpPolynomial&);
+RqPolynomial convolutionZq(const R2Polynomial&, const RqPolynomial&);
+RqPolynomial convolutionZq(const RpPolynomial&, const RqPolynomial&);
+RpPolynomial mods_p(RqPolynomial);
 
-struct ZpPolynomial {								// -Representation of the polynomials in Zp[x]/(x^N-1)
+struct RpPolynomial {								// -Representation of the polynomials in Zp[x]/(x^N-1)
 	enum Z3{_0_ = 0, _1_ = 1, _2_ = 2};					// -ZpCenterPolynomial are polynomials with coefficients in {-1, 0, 1}
 	private:
 	Z3* coefficients = NULL;
 	friend Encryption;
 
 	public:
-	ZpPolynomial();								// -Default constructor; initializes the polynomial with zeros
-	ZpPolynomial(const ZpPolynomial& P);
-	~ZpPolynomial() {
+	RpPolynomial();								// -Default constructor; initializes the polynomial with zeros
+	RpPolynomial(const RpPolynomial& P);
+	~RpPolynomial() {
 		if(this->coefficients != NULL) delete [] this->coefficients;
 	}
-	static ZpPolynomial randomTernary();
+	static RpPolynomial randomTernary();
 
 	private: void interchangeZeroFor(Z3 t);					// -Randomly selects a coefficient with value 0 and a coefficient with value t and interchanges them.
 	private: void changeZeroForOne();
 
 	public:
-	static ZpPolynomial getPosiblePrivateKey();				// -Setting d = N/3, returns a polynomial with d -1's and d+1 1's
+	static RpPolynomial getPosiblePrivateKey();				// -Setting d = N/3, returns a polynomial with d -1's and d+1 1's
 
-	ZpPolynomial& operator = (const ZpPolynomial& P);			// Assignment
+	RpPolynomial& operator = (const RpPolynomial& P);			// Assignment
 	int operator[](int i) const;
 	int degree() const;							// Returns degree of polynomial
 
-	friend ZpPolynomial mods_p(ZqPolynomial P);
-	friend ZqPolynomial convolutionZq(const Z2Polynomial&, const ZpPolynomial&);
-	friend ZqPolynomial convolutionZq(const ZpPolynomial&, const ZqPolynomial&);
+	friend RpPolynomial mods_p(RqPolynomial P);
+	friend RqPolynomial convolutionZq(const R2Polynomial&, const RpPolynomial&);
+	friend RqPolynomial convolutionZq(const RpPolynomial&, const RqPolynomial&);
 
 	mpz_class toNumber() const;						// -Interprests the coefficientes as a bese 3 number.
 	void print(const char* name = "", bool centered = true, const char* tail = "") const;
 	void println(const char* name = "", bool centered = true) const;
 };
 
-struct Z2Polynomial {								// Representation of the polynomials in Z2[x]/(x^N-1)
+struct R2Polynomial {								// Representation of the polynomials in Z2[x]/(x^N-1)
 	enum Z2 {_0_ = 0, _1_ = 1};						// Integers modulo 2 (binary numbers)
 	friend Z2 operator + (Z2 a,Z2 b) {					// Addition modulus 2
 		if(a!=b) return _1_;
@@ -77,33 +79,33 @@ struct Z2Polynomial {								// Representation of the polynomials in Z2[x]/(x^N-
 	friend Encryption;
 
 	public:
-	Z2Polynomial();
-	Z2Polynomial(const Z2Polynomial& P);
-	Z2Polynomial(const ZpPolynomial& P);
-	~Z2Polynomial() {
+	R2Polynomial();
+	R2Polynomial(const R2Polynomial& P);
+	R2Polynomial(const RpPolynomial& P);
+	~R2Polynomial() {
 		if(this->coefficients != NULL) delete[] this->coefficients;
 	}
 
-	Z2Polynomial& operator = (const Z2Polynomial& P);
-	Z2Polynomial& operator = (const ZpPolynomial& P);
-	Z2Polynomial& operator = (Z2 t);
-	Z2Polynomial  operator + (const Z2Polynomial&) const;			// In Z2, addition (+) coincide with subtraction (-)
-	Z2Polynomial  operator - (const Z2Polynomial&) const;			// In Z2, addition (+) coincide with subtraction (-)
-	Z2Polynomial  operator * (const Z2Polynomial&) const;
-	void division(const Z2Polynomial& P,Z2Polynomial res[2]) const;		// Division between this and P, result[2] will save the res[2]. Throws NTRU::MathException
-	Z2Polynomial gcdXNmns1(Z2Polynomial& thisBezout) const;			// Greatest common between this and x^N-1 polynomial. Writing
+	R2Polynomial& operator = (const R2Polynomial& P);
+	R2Polynomial& operator = (const RpPolynomial& P);
+	R2Polynomial& operator = (Z2 t);
+	R2Polynomial  operator + (const R2Polynomial&) const;			// In Z2, addition (+) coincide with subtraction (-)
+	R2Polynomial  operator - (const R2Polynomial&) const;			// In Z2, addition (+) coincide with subtraction (-)
+	R2Polynomial  operator * (const R2Polynomial&) const;
+	void division(const R2Polynomial& P,R2Polynomial res[2]) const;		// Division between this and P, result[2] will save the res[2]. Throws NTRU::MathException
+	R2Polynomial gcdXNmns1(R2Polynomial& thisBezout) const;			// Greatest common between this and x^N-1 polynomial. Writing
 										// gcd = u·(x^N - 1) + v·this, thisBezout == v
 	bool operator == (int t) const {
 		return this->degree() == 0 && this->coefficients[0] == t;
 	}
-	bool operator == (const Z2Polynomial& P) const;
+	bool operator == (const R2Polynomial& P) const;
 	bool operator != (int t) const {
 		return this->degree() != 0 || this->coefficients[0] != t;
 	}
 	Z2 operator [] (int i) const;
 
-	friend ZqPolynomial convolutionZq(const Z2Polynomial&, const ZpPolynomial&);
-	friend ZqPolynomial convolutionZq(const Z2Polynomial&, const ZqPolynomial&);
+	friend RqPolynomial convolutionZq(const R2Polynomial&, const RpPolynomial&);
+	friend RqPolynomial convolutionZq(const R2Polynomial&, const RqPolynomial&);
 
 	int  degree() const;
 	void print(const char* name = "", const char* tail = "") const;
@@ -116,29 +118,29 @@ struct Z2Polynomial {								// Representation of the polynomials in Z2[x]/(x^N-
 	}
 };
 
-ZqPolynomial operator - (int64_t, const ZqPolynomial&);				// The intention is to make this function a friend of ZqPolynomial
+RqPolynomial operator - (int64_t, const RqPolynomial&);				// The intention is to make this function a friend of RqPolynomial
 
-struct ZqPolynomial {								// Representation of the polynomials in Zq[x]/(x^N-1)
+struct RqPolynomial {								// Representation of the polynomials in Zq[x]/(x^N-1)
 	private:
 	int64_t* coefficients = NULL;
 	friend Encryption;
 
 	public:
-	ZqPolynomial();
-	ZqPolynomial(const ZqPolynomial& P);
-	ZqPolynomial(const char data[], int dataLength);			// -Initializing with string of bytes
-	~ZqPolynomial() {
+	RqPolynomial();
+	RqPolynomial(const RqPolynomial& P);
+	RqPolynomial(const char data[], int dataLength);			// -Initializing with string of bytes
+	~RqPolynomial() {
 		if(this->coefficients != NULL) delete[] this->coefficients;
 	}
-	ZqPolynomial& operator = (const ZqPolynomial& P);
+	RqPolynomial& operator = (const RqPolynomial& P);
 	int64_t operator[](int i) const;
-	ZqPolynomial operator + (const ZqPolynomial& P) const;
-	ZqPolynomial operator * (const ZqPolynomial& P) const;
-	friend ZqPolynomial operator - (int64_t, const ZqPolynomial&);
+	RqPolynomial operator + (const RqPolynomial& P) const;
+	RqPolynomial operator * (const RqPolynomial& P) const;
+	friend RqPolynomial operator - (int64_t, const RqPolynomial&);
 
-	friend ZqPolynomial convolutionZq(const Z2Polynomial&, const ZpPolynomial&);
-	friend ZqPolynomial convolutionZq(const Z2Polynomial&, const ZqPolynomial&);
-	friend ZqPolynomial convolutionZq(const ZpPolynomial&, const ZqPolynomial&);
+	friend RqPolynomial convolutionZq(const R2Polynomial&, const RpPolynomial&);
+	friend RqPolynomial convolutionZq(const R2Polynomial&, const RqPolynomial&);
+	friend RqPolynomial convolutionZq(const RpPolynomial&, const RqPolynomial&);
 
 	int degree()  const;							// -Returns degree of polynomial
 	bool equalsOne() const;
@@ -151,11 +153,11 @@ struct ZqPolynomial {								// Representation of the polynomials in Zq[x]/(x^N-
 										//  negative, +=q is applied in order to write a positive number
 	void print(const char* name = "", const char* tail = "") const;
 	void println(const char* name = "") const;
-	void save(const char* name = NULL, bool saveAsText = false) const;	// -Saving ZqPolynomial in a Binary file. Throws NTRU::FileIOException
-	static ZqPolynomial fromFile(const char* fileName);			// -Building a ZqPolynomial from file. Throws NTRU::FileIOException, NTRU::ParameterMismatchException
+	void save(const char* name = NULL, bool saveAsText = false) const;	// -Saving RqPolynomial in a Binary file. Throws NTRU::FileIOException
+	static RqPolynomial fromFile(const char* fileName);			// -Building a RqPolynomial from file. Throws NTRU::FileIOException, NTRU::ParameterMismatchException
 
 	private:
-	ZqPolynomial& timesThree();						// -Gets a ZpPolynomial via the operations 3p + 1
+	RqPolynomial& timesThree();						// -Gets a RpPolynomial via the operations 3p + 1
 };
 
 }
