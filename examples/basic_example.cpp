0166d3fb16d5126d179d9995bb4939dbd4a86ea2
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-07-25 16:23:50 -0600

    Updated file to match changes in source

--- examples/basic_example.cpp
+++ examples/basic_example.cpp
@@ -1,4 +1,5 @@
-#include"../Source/NTRUencryption.hpp"
+#include"../include/ntru/ntru.hpp"
+#include<iostream>
 
 int main(int argc, const char* argv[]) {
     if(argc > 1){
@@ -9,24 +10,24 @@ int main(int argc, const char* argv[]) {
     NTRU::Encryption ntru;                                                      // Create an encryption instance (generates keys automatically)
     const char* plainText = "Hello, quantum-resistant world!";                  // Encrypt a message
     std::cout << "Plain text: " << plainText << "\n\n";
-    // Create a ZpPolynomial instance. Using ZpPolynomial(const char data[], int dataLength, bool isPlainText)
-    NTRU::ZpPolynomial ZpPolyPlainText = NTRU::Encryption::ZpPolynomialFromBytes(plainText, strlen(plainText)+1, false);
+    // Create a RpPolynomial instance. Using RpPolynomial(const char data[], int dataLength, bool isPlainText)
+    NTRU::RpPolynomial ZpPolyPlainText = NTRU::Encryption::RpPolynomialFromBytes(plainText, strlen(plainText)+1, false);
     ZpPolyPlainText.println("Plain Text (vector form)");
 
     // Starting with encryption-decryption phase.
-    NTRU::ZqPolynomial ciphertext = ntru.encrypt(ZpPolyPlainText);              // Using encrypt(const ZqPolynomial&)
+    NTRU::RqPolynomial ciphertext = ntru.encrypt(ZpPolyPlainText);              // Using encrypt(const RqPolynomial&)
     std::cout << '\n';
     ciphertext.println("Cipher Text (vector form)");
-    NTRU::ZpPolynomial decrypted = ntru.decrypt(ciphertext);                    // Decrypt the message. Using decrypt(const ZqPolynomial&)
+    NTRU::RpPolynomial decrypted = ntru.decrypt(ciphertext);                    // Decrypt the message. Using decrypt(const RqPolynomial&)
     std::cout << '\n';
     decrypted.println("Decrypted message (vector form)");
     // Finishing with encryption-decryption phase.
 
     char decryptedBytes[1024];
-    NTRU::Encryption::ZpPolynomialtoBytes(decrypted, decryptedBytes, false);    // Convert back to bytes for your application
+    NTRU::Encryption::RpPolynomialtoBytes(decrypted, decryptedBytes, false);    // Convert back to bytes for your application
     std::cout << "\nDecrypted message: " << decryptedBytes << '\n';
 
-    mpz_class num_r = decrypted.toNumber();                                     // Showing the representation of ZpPolynomial through a number
+    mpz_class num_r = decrypted.toNumber();                                     // Showing the representation of RpPolynomial through a number
     std::cout << "\nNumber associated with (polynomial) decrypted message:\n" << num_r << '\n' << std::endl;
     return 0;
 }
