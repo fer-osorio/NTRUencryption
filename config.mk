b6385d90015ec87dc9b2a4ff8146a1f6d1c475f4
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-07-26 22:37:07 -0600

    Defined macros NTRU_N and NTRU_Q. Add compilation flags

--- config.mk
+++ config.mk
@@ -2,12 +2,22 @@
 CC = gcc
 CXX = g++
 AR = ar
+
+# NTRU Parameters (can be overridden on command line)
+NTRU_N ?= 509
+NTRU_q ?= 2048
+
 WARNINGS = -Wall -Weffc++ -Wextra -Wsign-conversion -pedantic-errors
 DEBUG    = -ggdb -fno-omit-frame-pointer
 OPTIMIZE = -O2
 STANDARD = -std=c++2a
+
+# Base compiler flags
 COMPILE_FLAGS = $(WARNINGS) $(DEBUG) $(OPTIMIZE) $(STANDARD)
 LDFLAGS = -lgmp -lgmpxx
+
+# Add NTRU parameters as preprocessor defines
+CXXFLAGS += -DNTRU_N=$(NTRU_N) -DNTRU_q=$(NTRU_q) -DNTRU_P=$(NTRU_P)
 
 # Directories
 BUILD_DIR = build
