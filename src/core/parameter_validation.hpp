1606203a9b190d219ab2604e6d025548c82f6b02
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-07-26 20:49:36 -0600

    Add enumerations, macros _N_ -> NTRU_N, _q_ -> NTRU_Q

--- src/core/parameter_validation.hpp
+++ src/core/parameter_validation.hpp
@@ -1,26 +1,29 @@
 #include<cstdint>
-#include"../../include/ntru/parameters.hpp"
 
 #ifndef PARAMETER_VALIDATION_HPP
 #define PARAMETER_VALIDATION_HPP
 
-#ifndef _N_
-    #define _N_ 701
+enum ntru_N {_509_  = 509,  _677_  = 677,  _701_  = 701,  _821_ = 821, _1087_ = 1087, _1171_ = 1171, _1499_ = 1499 };	// All the possible values for the N
+enum ntru_q {_2048_ = 2048, _4096_ = 4096, _8192_ = 8192 };			// All the possible values for the q
+//enum ntru_p {_3_= 3 };
+
+#ifndef NTRU_N
+    #define NTRU_N 701
 #endif
-#ifndef _q_
-    #define _q_ 8192
+#ifndef NTRU_Q
+    #define NTRU_Q 8192
 #endif
 
-static int log2_q(NTRU_q q) {                                              // -Returns logarithm base 2 of a NTRU_q value
+static int log2_q(ntru_q q) {                                              // -Returns logarithm base 2 of a ntru_q value
     int log2q = 0, qq = q;
     for(; qq > 1; qq >>= 1, log2q++) {}
     return log2q;
 }
 
-int64_t q_1     = (int64_t)_q_-1;										// Will hold q-1, this will help with mod q operation
+int64_t q_1     = (int64_t)NTRU_Q-1;										// Will hold q-1, this will help with mod q operation
 int64_t negq_1  = ~q_1;													// This will help in the centering process. Set as ~q_1
-int64_t q_div_2 = _q_>>1;
-int     log2q   = log2_q((NTRU_q)_q_);
+int64_t q_div_2 = NTRU_Q>>1;
+int     log2q   = log2_q((ntru_q)NTRU_Q);
 
 int64_t modq(int64_t t) {											// operation t % q
     if(t >= 0)	return t & q_1;									// Equivalent to t % q since q is a power of 2
