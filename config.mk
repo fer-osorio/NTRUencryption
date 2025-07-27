280bf8c5c142b6c848e6aa2fffcbe7b98159dd05
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-07-27 15:39:44 -0600

    COMPILE_FLAGS -> CXXFLAGS

--- config.mk
+++ config.mk
@@ -13,11 +13,11 @@ OPTIMIZE = -O2
 STANDARD = -std=c++2a
 
 # Base compiler flags
-COMPILE_FLAGS = $(WARNINGS) $(DEBUG) $(OPTIMIZE) $(STANDARD)
+CXXFLAGS = $(WARNINGS) $(DEBUG) $(OPTIMIZE) $(STANDARD)
 LDFLAGS = -lgmp -lgmpxx
 
 # Add NTRU parameters as preprocessor defines
-CXXFLAGS += -DNTRU_N=$(NTRU_N) -DNTRU_q=$(NTRU_q) -DNTRU_P=$(NTRU_P)
+CXXFLAGS += -DNTRU_N=$(NTRU_N) -DNTRU_q=$(NTRU_q) #-DNTRU_P=$(NTRU_P)
 
 # Directories
 BUILD_DIR = build
