36bc4380db9b97a9a08bdf8ccdb7b2a42fce7e29
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-07-31 15:07:17 -0600

    Add include guards

--- include/ntru/ntru.hpp
+++ include/ntru/ntru.hpp
@@ -1,3 +1,8 @@
 // Contains all other headers
+#ifndef _NTRU_HPP_
+#define _NTRU_HPP_
+
 #include"exception"
 #include"encryption.hpp"	// -encryption.hpp contains polynomials.hpp and statistical_measures.hpp
+
+#endif
