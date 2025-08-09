9597432bd9d1ce8359acd6d74899936a0f4a4e75
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-08 18:08:24 -0600

    Fix error. Define macro NUMBER_OF_ROUNDS. Delete commnet.

--- src/statistics/timing.cpp
+++ src/statistics/timing.cpp
@@ -5,19 +5,20 @@
 #include<chrono>
 #include<memory>
 
-#define NUMBER_OF_ROUNDS 4096U
+#define NUMBER_OF_ROUNDS 1024U
 
 template<typename T> static std::vector<uint32_t> run_test(std::function<T()> func, TestID tc) {
     std::chrono::steady_clock::time_point begin;
     std::chrono::steady_clock::time_point end;
     std::vector<uint32_t> time_measures(NUMBER_OF_ROUNDS);
-    for(int i = 0; i < NUMBER_OF_ROUNDS; i++){
-        if((i & 65) == 0) std::cout << tc.get_label() << " test: Round "<< i << std::endl;
+    for(size_t i = 0; i < NUMBER_OF_ROUNDS; i++){
+        if((i & 0xFF) == 0) std::cout << tc.get_label() << " test: Round "<< i << std::endl;
         begin = std::chrono::steady_clock::now();
-        func();//e.setKeys();// arr_ptr_methods[tc.get_ID]()
+        func();
         end  = std::chrono::steady_clock::now();
         time_measures[i] = (uint32_t)std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
     }
+    std::cout << tc.get_label() << ". Total number of rounds completed: " << NUMBER_OF_ROUNDS << std::endl;
     return time_measures;
 }
 
@@ -30,10 +31,10 @@ Encryption::encryption_test(const TestID& t_id,
                             const NTRU::RqPolynomial& emsg){
     std::vector<uint32_t> time_measures;
     std::cout << t_id.get_label() << " test: Parameters N = "<< NTRU_N << ", q = " << NTRU_Q << " --------------" << std::endl;
-    std::unique_ptr<NTRU::Encryption> ptr_e;
+    std::unique_ptr<NTRU::Encryption> ptr_e = nullptr;
     switch(t_id.get_ID_num()){
         case TestIDnum::KEY_GENERATION:
-            ptr_e = std::make_unique<NTRU::Encryption>(new NTRU::Encryption(e));
+            ptr_e = std::make_unique<NTRU::Encryption>(NTRU::Encryption(e));
             time_measures = run_test<int>([&]()->int{ ptr_e->setKeys(); return 0; }, t_id);
             break;
         case TestIDnum::CIPHERING:
