62281d5a49054c48afbe852c6f44d82fb0827691
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-10 12:53:58 -0600

    Fix bug. Remove using namespace statement.

--- src/metrics-analysis/data_generation.cpp
+++ src/metrics-analysis/data_generation.cpp
@@ -1,20 +1,18 @@
 #include"../../include/metrics-analysis/data_generation.hpp"
 
-using namespace DataGeneration;
+std::vector<std::byte> DataGeneration::dataSource(std::function<std::byte (size_t)> generator, size_t size){
+    std::vector<std::byte> data(size);
+    for(size_t i = 0; i < size; i++) data[i] = generator(i);
+    return data;
+}
 
-std::vector<std::byte> simpleDataSource(size_t size){
+std::vector<std::byte> DataGeneration::simpleDataSource(size_t size){
     return dataSource(
         [](size_t i) ->std::byte {
             size_t FF = 0xFF, q = (i & ~FF)>>8, r = i & FF;                     // i = 256·q + r
             return std::byte((q+r) & FF);                                       // (q+i) % 256
         }
         ,size);
-}
-
-std::vector<std::byte> dataSource(std::function<std::byte (size_t)> generator, size_t size){
-    std::vector<std::byte> data(size);
-    for(size_t i = 0; i < size; i++) data[i] = generator(i);
-    return data;
 }
 
 
