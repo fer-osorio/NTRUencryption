005e75dcdfb1a5efc1e9309747bbc94294584fdd
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-08-10 01:53:35 -0600

    Add functions for objects byte-view

--- src/utils/debug_helpers.cpp
+++ src/utils/debug_helpers.cpp
@@ -3,8 +3,142 @@
 #include<cstring>
 #include"../../include/print_debug/debug_helpers.hpp"
 
-#ifndef _DEBUG_HELPERS_
-#define _DEBUG_HELPERS_
+#define BYTE_UPPER_TWO_BITS 0xC0     // binary -->1100,0000
+/*
+    //  View the passed object as an array of bytes, print the entire array using the specified base.
+    //  Returns: Number of characters printed.
+*/
+template <typename T>
+static size_t print_obj_byte_view(const T& obj, NumberBaseTwosPower::Base b){
+    const unsigned char* byte_ptr = reinterpret_cast<const unsigned char*>(&obj); // Allows to view the object as an array of unsigned chars
+    const size_t obj_sz = sizeof(obj);
+    unsigned int buff = 0, lower_bits = static_cast<unsigned int>(b), bit_mask = 0, digit;
+    const int bit_num = static_cast<int>(NumberBaseTwosPower::max_bit_amount(b));
+    int right_displacement;                                                     //  Necessary amount of right bit shift to move the masked bits to the lower end.
+    size_t num_printed_chars = 0;                                               //  Number of printed chars
+
+    for(int i = obj_sz-1; i >= 0; i--){
+        buff = byte_ptr[i];
+        bit_mask = NumberBaseTwosPower::to_upper_bits(b);
+        right_displacement = 8 - bit_num;
+        if(b == NumberBaseTwosPower::Base::OCTAL) {                             //  Special case with octal base. A byte can be represented with three octal
+            right_displacement = 6;                                             //  digits, but the left-most digit of this octal-representation is formed with the
+            std::cout << ((buff & BYTE_UPPER_TWO_BITS) >> right_displacement);  //  two left-most bits of the byte (supposing the byte has its most significant
+            bit_mask >>= 2;                                                     //  bits to the left).
+            right_displacement -= bit_num;
+            num_printed_chars += 2;
+        }
+        while(bit_mask >= lower_bits){
+            digit = (buff & bit_mask) >> right_displacement;                    //  In each cycle, taking the corresponding (to the cycle) bits.
+            if(digit < 10) std::cout << digit;
+            else std::cout << static_cast<char>(digit + 55);                    //  In this case, print a hexadecimal character.
+            bit_mask >>= bit_num;                                               //  Moving (inside the byte) to the next set of bits.
+            right_displacement -= bit_num;
+            num_printed_chars++;
+        }
+        if(i > 0){
+            std::cout  << ',';                                                  //  Printing comma for better readabitily.
+            num_printed_chars += 2;
+        }
+    }
+    return num_printed_chars;
+}
+
+/*
+    //  Prints the elements of interval specified by the at_index and slice_with parameters.
+    //  arg at_index: Intended as the center of the specified interval, but there will be cases were this is not possible. As long as is smaller than v.size(), is
+    //      guarantee to be printed.
+    //  arg slice_with: Number of elements to be printed.
+    //  Return: Viewing the printed output as a sequence of characters, returns the index of the starting character of the element specified by at_index.
+*/
+template <typename T>
+static size_t print_slice_centered(const std::vector<T>& v, size_t at_index, size_t slice_width, NumberBaseTwosPower::Base b){
+    if(slice_width > v.size()) slice_width = v.size();                          // Ensuring 'slice_width <= v.size()' inequality
+    if(at_index >= v.size()) return 0;                                          // Ensuring 'at_index < v.size()' inequality
+    size_t sw_div2 = slice_width >> 1, sw_mod2 = slice_width & 1;               // Equivalent to slice_width / 2 and slice_width % 2
+    size_t first_ind = 0;
+    size_t last_ind  = 0;
+    size_t obj_char_len = 0;
+    size_t at_index_obj_poss = 0;
+
+    // The idea is to have the output centered at 'at_index', but sometimes it will not be possible. The following if's handle those cases.
+    if(sw_div2 >= at_index){                                                    // First case, not enough space to the left
+        first_ind = 0;
+        last_ind = slice_width;
+    }else{
+        if(at_index + sw_div2 + sw_mod2 >= v.size()){                           //  Second case, not enough space to the right.
+            last_ind = v.size();                                                //  Notice how, in the impair width case, we are tilting the output to the right.
+            first_ind = v.size() - slice_width;
+        }else{                                                                  //  Third case, enough space in both sides.
+            first_ind = at_index - sw_div2;
+            last_ind = at_index + sw_div2 + sw_mod2;                            //  As mentioned before, in the impair width case, we are tilting the output to the
+        }                                                                       //  right.
+    }
+
+    if(first_ind != 0) std::cout << "...";                                      //  Indicate that the array continues to the left
+    else std::cout << "[  ";
+    at_index_obj_poss += 3;
+    // Sending coefficients to standard output.
+    for(size_t i = first_ind, last_ind_1 = last_ind-1; i < last_ind; i++){
+        obj_char_len = print_obj_byte_view(v[i], b);
+        if(i < at_index) at_index_obj_poss += obj_char_len;
+        if(i != last_ind_1){
+            std::cout << ' ';                                                   //  Printing space for better readability
+            if(i <= at_index) at_index_obj_poss++;
+        }
+    }
+    if(last_ind != v.size()) std::cout << "...";                                //  Indicate that the array continues to the right
+    else std::cout << "  ]";
+
+    return at_index_obj_poss;
+}
+
+/*
+    //  Prints an interval of legth 'with' were the first difference [if exist] is found. If possible, the element were the first difference is found will be
+    //      in the center of the interval.
+    //  arg v1_slice_front: String printed in front (right) to the slice obtained from v1
+    //  arg v2_slice_front: String printed in front (right) to the slice obtained from v2
+    //  Returns:
+    //      Case -2: Vectors are equal.
+    //      Case -1: Vectors have different sizes.
+    //      Case ">= 0": Vectors are different at the returned index.
+*/
+static int print_first_difference(std::vector<char> v1, std::vector<char> v2, std::string diff_msg, std::string v1_slice_front, std::string v2_slice_front, size_t width){
+    if(v1 == v2) return -2;
+    size_t s1 = v1.size(), s2 = v2.size();
+    if(s1 != s2) {
+        std::cout << "Vectors differ in size. First vector size: " << s1 << ", second vector size: " << s2 << "\n";
+        return -1;
+    }
+    size_t at_ind_elem_pos = 0;
+    int num_spaces = 0;
+    std::string first_occurr_msg = "Difference here";
+    size_t left_offset = std::max( std::max(v1_slice_front.size(), v2_slice_front.size()), first_occurr_msg.size());
+    size_t v1front_left_offset = left_offset - v1_slice_front.size();
+    size_t v2front_left_offset = left_offset - v2_slice_front.size();
+    size_t i;
+    first_occurr_msg += " ~~^";                                                 //  This message will allow us to see the exact possition of the first difference
+    for(i = 0; i < s1; i++){
+        if(v1[i] != v2[i]){
+            std::cout << diff_msg << '\n';
+            std::cout << "Firs inequality found at index " << std::to_string(i) << '\n';
+            std::cout << v1_slice_front;
+            for(; v1front_left_offset > 0; v1front_left_offset--) std::cout << ' '; //  Padding to get an aligned output.
+            at_ind_elem_pos = print_slice_centered(v1, i, width, NumberBaseTwosPower::Base::HEXADECIMAL);
+            std::cout << '\n';
+            std::cout << v2_slice_front;
+            for(; v2front_left_offset > 0; v2front_left_offset--) std::cout << ' '; //  Padding to get an aligned output.
+            print_slice_centered(v2, i, width, NumberBaseTwosPower::Base::HEXADECIMAL);
+            std::cout << '\n';
+            num_spaces = left_offset + at_ind_elem_pos - first_occurr_msg.length();
+            for(; num_spaces > 0; num_spaces--) std::cout << ' ';
+            std::cout << first_occurr_msg << std::endl;                         //  Indicating were the difference is.
+            break;
+        }
+    }
+    return static_cast<int>(i);
+}
+
 
 static void printByteArrayBin(const char byteArray[], size_t size){             // -Prints an array of bytes with no line break.
     uint8_t bit = 0x80;                                                         // -Byte 1000,0000
@@ -91,5 +225,3 @@ void displayByteArrayBin(const char byteArray[], size_t size){                  
 void displayByteArrayChar(const char byteArray[], size_t size){                 // -Prints each byte of array as a character or hexadecimal value
     displayByteArray(byteArray, size, 20, "char");
 }
-
-#endif
