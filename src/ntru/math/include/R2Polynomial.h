64c98c7a6d85407f9844f4da478fd6985c60f91a
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-10-30 13:48:58 -0600

    Delete structure definition

--- src/ntru/math/include/R2Polynomial.h
+++ src/ntru/math/include/R2Polynomial.h
@@ -6,13 +6,13 @@ extern "C" {
 #include "exceptions.h"
 #include <stdint.h>
 
-typedef union R2Polynomial_t{
-  uint8_t coeffs[NTRU_N];
-  uint64_t coeffs_64[NTRU_N/64 + 1];
-} R2Polynomial_t;
+typedef union R2Polynomial_t R2Polynomial_t;
 typedef const union R2Polynomial_t* ptrR2Polynomial_t;
 
+// Get and write.
 enum ExceptionCode R2Polynomial_t_getFromArrayPointer(const uint8_t* source, R2Polynomial_t* dest);
+enum ExceptionCode R2Polynomial_t_writeOnArrayPointer(ptrR2Polynomial_t source, uint8_t* dest);
+
 enum ExceptionCode R2Polynomial_t_convolution(ptrR2Polynomial_t input1, ptrR2Polynomial_t input2, R2Polynomial_t* output);
 enum ExceptionCode R2Polynomial_t_division(ptrR2Polynomial_t divident, ptrR2Polynomial_t divisor, R2Polynomial_t* quotient, R2Polynomial_t* remainder);
 enum ExceptionCode R2Polynomial_t_computePseudoInverse(ptrR2Polynomial_t input, R2Polynomial_t* destGCD, R2Polynomial_t* destPseudoInverse);
