7c5da9b316e03be16a52281d84fa1719fb9248b1
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-10-30 15:57:31 -0600

    Create file. Defining methods for RpPolynomial handling

---
+++ src/ntru/math/src/RpPolynomial.c
@@ -0,0 +1,21 @@
+#include "../include/RpPolynomial.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct RpPolynomial_t{
+  uint8_t coeffs[NTRU_N];
+};
+
+enum ExceptionCode RpPolynomial_t_getCopyFromArray(const uint8_t* source, RpPolynomial_t* dest){
+  if(source == NULL) return NullSource;
+  if(dest   == NULL) return NullDestination;
+  for(size_t i = 0; i < NTRU_N; i++) dest->coeffs[i] = source[i];
+  return NoException;
+}
+
+enum ExceptionCode RpPolynomial_t_writeOnArrayPointer(ptrRpPolynomial_t source, uint8_t* dest){
+  if(source == NULL) return NullSource;
+  if(dest   == NULL) return NullDestination;
+  for(size_t i = 0; i < NTRU_N; i++) dest[i] = source->coeffs[i];
+  return NoException;
+}
