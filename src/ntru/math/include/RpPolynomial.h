8470602965cd385878ba8ad47d78d2b62f289a7b
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-10-30 15:53:59 -0600

    Create file. Handling polynomials in Rp

---
+++ src/ntru/math/include/RpPolynomial.h
@@ -0,0 +1,19 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../../../../include/ntru/parameters_constants.hpp"
+#include "exceptions.h"
+#include <stdint.h>
+
+typedef struct RpPolynomial_t RpPolynomial_t;
+typedef const struct RpPolynomial_t* ptrRpPolynomial_t;
+
+// Get and write.
+enum ExceptionCode RpPolynomial_t_getCopyFromArray(const uint16_t* source, RpPolynomial_t* dest);
+enum ExceptionCode RpPolynomial_t_writeCopyOnArray(ptrRpPolynomial_t source, uint16_t* dest);
+
+//enum ExceptionCode RpPolynomial_t_multiply(ptrRpPolynomial_t input1, ptrRpPolynomial_t input2, RpPolynomial_t* output);
+#ifdef __cplusplus
+}
+#endif
