2140b2be695e5201ba03771597e1c96266508994
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-10-30 15:42:16 -0600

    Rename structure and functions

--- src/ntru/math/src/R0Polynomial.c
+++ src/ntru/math/src/R0Polynomial.c
@@ -1,31 +1,31 @@
-#include "../include/RqPolynomial.h"
+#include "../include/R0Polynomial.h"
 #include "../include/ZqInteger.h"
 #include <stdlib.h>
 #include <stdbool.h>
 
-struct RqPolynomial_t{
+struct R0Polynomial_t{
   uint16_t coeffs[NTRU_N];
 };
 
-enum ExceptionCode RqPolynomial_t_getFromArrayPointer(const uint16_t* source, RqPolynomial_t* dest){
+enum ExceptionCode R0Polynomial_t_getFromArrayPointer(const uint16_t* source, R0Polynomial_t* dest){
   if(source == NULL) return NullSource;
   if(dest   == NULL) return NullDestination;
   for(size_t i = 0; i < NTRU_N; i++) dest->coeffs[i] = source[i];
   return NoException;
 }
 
-enum ExceptionCode RqPolynomial_t_writeOnArrayPointer(ptrRqPolynomial_t source, uint16_t* dest){
+enum ExceptionCode R0Polynomial_t_writeOnArrayPointer(ptrR0Polynomial_t source, uint16_t* dest){
   if(source == NULL) return NullSource;
   if(dest   == NULL) return NullDestination;
   for(size_t i = 0; i < NTRU_N; i++) dest[i] = source->coeffs[i];
   return NoException;
 }
 
-static void RqPolynomial_t_modq(ptrRqPolynomial_t input, RqPolynomial_t* output){
+static void R0Polynomial_t_modq(ptrR0Polynomial_t input, R0Polynomial_t* output){
   for(size_t i = 0; i < NTRU_N; i++) output->coeffs[i] = MODQ(input->coeffs[i]);
 }
 
-enum ExceptionCode RqPolynomial_t_multiply(ptrRqPolynomial_t input1, ptrRqPolynomial_t input2, RqPolynomial_t* output){
+enum ExceptionCode R0Polynomial_t_multiply(ptrR0Polynomial_t input1, ptrR0Polynomial_t input2, R0Polynomial_t* output){
   int i, j, k;
   for(i = 0; i < NTRU_N; i++) {
     k = NTRU_N - i;
@@ -34,5 +34,5 @@ enum ExceptionCode RqPolynomial_t_multiply(ptrRqPolynomial_t input1, ptrRqPolyno
     for(k = 0; k < i; j++, k++)                                                 // Using the definition of convolution polynomial ring
       output->coeffs[k] += input1->coeffs[i] * input2->coeffs[j];
   }
-  RqPolynomial_t_modq(output,output);                                           // Applying mod q
+  R0Polynomial_t_modq(output,output);                                           // Applying mod q
 }
