ea0b15b1c21273148ff8f07f6f8b48f6bbf7830e
fer-osorio <alexis.fernando.osorio.sarabio@gmail.com>
2025-10-30 14:01:15 -0600

    Create file for RqPolynomials

---
+++ src/ntru/math/include/RqPolynomial.h
@@ -0,0 +1,20 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../../../../include/ntru/parameters_constants.hpp"
+#include "exceptions.h"
+#include <stdint.h>
+
+typedef struct RqPolynomial_t RqPolynomial_t;
+typedef const struct RqPolynomial_t* ptrRqPolynomial_t;
+
+// Get and write.
+enum ExceptionCode RqPolynomial_t_getFromArrayPointer(const uint16_t* source, RqPolynomial_t* dest);
+enum ExceptionCode RqPolynomial_t_writeOnArrayPointer(ptrRqPolynomial_t source, uint16_t* dest);
+
+enum ExceptionCode RqPolynomial_t_convolution(ptrRqPolynomial_t input1, ptrRqPolynomial_t input2, RqPolynomial_t* output);
+
+#ifdef __cplusplus
+}
+#endif
